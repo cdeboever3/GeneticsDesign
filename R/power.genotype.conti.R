1d468c0a12e46e43a27ce8179cd4e60dc2e213ba
greg@warnes.net <greg@warnes.net@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-09-28 19:21:56 +0000

    Expose power.genotype.conti() function for estimating power for genetice studies using baseline measurements via simulation.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GeneticsDesign@27610 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/power.genotype.conti.R
+++ R/power.genotype.conti.R
@@ -1,52 +1,3 @@
-power.casectrl.plot <- function (N, gamma=1.6, p=1:9/10, kp=0.1, alpha=0.05, fc=0.5,
-                                 minh=c('multiplicative', 'dominant','recessive'),
-                                 Nsnp=1, vary=c('prevalence','SNPs'), ylim=c(0,1), PLOT=T, ... )
-{
-  minh <- match.arg(minh)
-  vary <- match.arg(vary)
-  if (length(p)<2) stop('Must have more than 1 value in p.')
-  if (length(kp) > 1 & length(Nsnp) > 1) stop("Nsnps and kp can't be all > 1.")
-  if (vary=='prevalence') {
-    cmd <- expression(tapply(p, p, function(x, ...) power.casectrl(p=x,...), N=N, gamma=gamma, kp=kp[j], alpha=alpha/Nsnp, fc=fc, minh=minh))
-    Xvary <- kp 
-  } else if (vary=='SNPs') {
-    cmd <- expression(tapply(p, p, function(x, ...) power.casectrl(p=x,...), N=N, gamma=gamma, kp=kp, alpha=alpha/Nsnp[j], fc=fc, minh=minh))
-    Xvary <- Nsnp 
-  }
-  J <- length(Xvary)
-  ret <- matrix(NA, nc=J, nr=length(p))
-  colnames(ret) <- paste(vary, '=', Xvary)
-  for (j in 1:J) ret[,j] <- eval(cmd)
-
-  if (PLOT) {
-    nc <- 1:ncol(ret)
-    subt <- paste("( RR", gamma, "; total subjects", N,"; SNPs", Nsnp[1], "; prevalence", kp[1],
-                   "; mode of inheritance:", minh, "; overall sig.level", alpha, ")" )
-    matplot(p, ret, type="l", ylim=ylim, lty=1, col=nc, xlab="Allele Frequency", ylab="Power", sub=subt, ...)
-    abline(h=c(.8), lty=1)
-    legend( locator(1), colnames(ret), lty=1, col=nc )
-  }
-  ret
-}
-
-
-### simple simulation for two group design
-pw <- function(n1, n2=n1*(1-fc)/fc, fc=.5, pi=0, me1=50, me2=45, sd1=10, sd2=10, TEST=F){
-  covm <- matrix(c(1,    pi,    pi,    1   ), nr=2)*
-          matrix(c(sd1^2, sd1*sd2, sd1*sd2, sd2^2), nr=2)
-  x1 <- rmvnorm(n=n1, mean=c(me1,me1), sigma=covm)
-  x2 <- rmvnorm(n=n2, mean=c(me1,me2), sigma=covm)
-  x <- data.frame(rbind(x1,x2), Trt=c(rep(0,n1), rep(1,n2)))
-  colnames(x) <- c('X', 'Y', 'Trt')
-  mod <- lm(Y~X+Trt, data=x)
-  if (TEST) {
-    print( summary(mod) )
-    plot(Y~X+Trt, data=x)
-  }
-  ret <- anova(mod, test='F')['Trt','Pr(>F)']
-  ret
-}
-
 # power calculation for studies using baseline measure 
 #   - simulation: continuous response, baseline and genotype as covariate (ANCOVA)
 #   - can specify various modes of inheritance ('additive', 'dominant','recessive')
@@ -72,8 +23,37 @@ pw <- function(n1, n2=n1*(1-fc)/fc, fc=.5, pi=0, me1=50, me2=45, sd1=10, sd2=10,
 #     statistically inefficient: a simulation study" BMC Med Res Methodol. 2001; 1 (1): 6
 # requirement: need library 'mvtnorm'
 
-simu.genotype.conti <- function (N, p=0.15, pi=0, me1=50, me2=me1, delta=-5, sd1=10, sd2=10, TEST=F,
-                                 minh=c('additive', 'dominant','recessive'), genotype.delta=T, Factor=F) 
+### power calculation
+power.genotype.conti <- function(N, Rep=2000, alpha=.05, ...){
+  pval <- sapply(rep(N, Rep), FUN=simu.genotype.conti, ...)
+  retval <- list()
+  retval$power <- mean(pval<=alpha)
+  retval$ci    <- ci.binom(pval<=alpha)
+  retval$call  <- match.call()
+  class(retval) <- "power.genoytpe.conti"
+  retval
+}
+
+print.power.genoytpe.conti <- function(x, ...)
+  {
+    cat("\n")
+    cat("Power calculation for a study including genotyping\n")
+    cat("\n")
+    cat("call:\n")
+    print(x$call)
+    cat("\n")
+    cat("Estimated power:", x$power, "\n")
+    cat("\n")
+    print(x$power)
+    print("Simulation confidence region for estimated power:\n")
+    cat("\n")
+    print(x$ci)
+  }
+
+
+### simulation
+simu.genotype.conti <- function (N, p=0.15, pi=0, me1=50, me2=me1, delta=-5, sd1=10, sd2=10, verbose=FALSE,
+                                 minh=c('additive', 'dominant','recessive'), genotype.delta=TRUE, Factor=FALSE) 
 {
   minh <- match.arg(minh)
   if ( min(N, sd1, sd2)<0 ) stop('N, sd1, and sd2 must be greater than 0')
@@ -107,7 +87,7 @@ simu.genotype.conti <- function (N, p=0.15, pi=0, me1=50, me2=me1, delta=-5, sd1
   if (Factor) x$Trt <- as.factor(x$Trt)
   colnames(x) <- c('X', 'Y', 'Trt')
   mod <- lm(Y~X+Trt, data=x)   # ANCOVA
-  if (TEST) {
+  if (verbose) {
     print( summary(mod) )
     plot(Y~X+Trt, data=x)
   }
@@ -115,11 +95,6 @@ simu.genotype.conti <- function (N, p=0.15, pi=0, me1=50, me2=me1, delta=-5, sd1
   ret
 }
 
-### power calculation
-power.genotype.conti <- function(N, Rep=100, alpha=.05, ...){
-  pval <- sapply(rep(N, Rep), FUN=simu.genotype.conti, ...)
-  power <- length(pval[pval<=alpha])/length(pval)
-  power
-}
+
 
 
